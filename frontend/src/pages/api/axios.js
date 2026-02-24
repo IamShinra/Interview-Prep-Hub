e3b5951c28c6b7f9eb865969902caaeee0ac191b
Shinra Zen <pratham18koli@gmail.com>
2026-02-25 00:32:47 +0530

    updating files

--- frontend/src/pages/api/axios.js
+++ frontend/src/pages/api/axios.js
@@ -4,4 +4,6 @@ const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://interview-prep-hub-dpt5.onrender.com",
 });
 
+console.log("Axios Base URL:", instance.defaults.baseURL);
+
 export default instance;
